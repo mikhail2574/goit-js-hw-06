89a901d21672dade80f7262e6c9405ab1bdbbe57
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-08-05 02:39:01 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+let creating = document.querySelector("[data-create]");
+let deleting = document.querySelector("[data-destroy]");
+let inputValue = document.querySelector("[type='number']");
+let container = document.getElementById("boxes");
+
+creating.addEventListener("click", (event) => {
+  for (let i = 0; i < parseInt(inputValue.value); i++) {
+    let box = document.createElement("div");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = 30 + 10 * i + "px";
+    box.style.height = 30 + 10 * i + "px";
+    container.append(box);
+  }
+});
+
+function destroyBoxes() {
+  container.innerHTML = "";
+}
+
+deleting.addEventListener("click", destroyBoxes);
